367cc176333a0347a505dacf991e1ad169877178
Ethan Thierry <45918948+IchiiDev@users.noreply.github.com>
2021-02-26 14:48:41 +0100

    Adding NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <NavBar :logged="true" :account="Ichii" />
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  body {
+    margin: 0;
   }
-}
 </style>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import NavBar from '@/components/NavBar.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    NavBar,
+  },
+})
+export default class Home extends Vue {}
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="navbar">
+        <p v-if="!logged">You need to be logged in to continue</p>
+        <div class="content" v-else>
+            <p id="room">Room: {{ room }}</p>
+            <p id="account">Test: {{ account }}</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    #navbar {
+        width: 100%;
+        height: 4em;
+        background-color: #2C2F33;
+        color: #99AAB5;
+        display: flex;
+        justify-content: center;
+    }
+    .content {
+        background-color: white;
+        display: flex;
+        width: 100%;
+    }
+    #room {
+        justify-self: left;
+    }
+    #account {
+        justify-self: right;
+    }
+</style>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+@Component
+export default class NavBar extends Vue {
+  @Prop() public logged = true;
+  @Prop() public account!: string;
+  @Prop() public room!: string;
+}
+</script>
